087ddcd1bf332c0daab56839b9be0606fb7f1d21
llakostass <103038340+llakostass@users.noreply.github.com>
2022-11-16 20:01:10 +0300

    Update and rename Домашка2/src/DogTest.java to Домашка2/Src/DogTest.java

--- Домашка2/Src/DogTest.java
+++ Домашка2/Src/DogTest.java
@@ -2,12 +2,12 @@ public class DogTest
 {
     public static void main(String[] args)
     {
-        Dog d1 = new Dog("Bebrick",5);
+        Dog d1 = new Dog("Dzeki",5);
         System.out.println(d1.toString());
         System.out.println(d1.getAge());
         System.out.println(d1.getName());
         d1.setAge(8);
-        d1.setName("Otbitiy");
+        d1.setName("Julian");
         System.out.println(d1.toString());
         System.out.println(d1.human_age());
     }
